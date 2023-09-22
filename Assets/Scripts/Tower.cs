4f917435de8aa681e0ca9dd492973425084d86af
Jeffrey-hue <76977066+Jeffrey-hue@users.noreply.github.com>
2023-09-22 14:24:41 -0400

    added first tower and wave spawner

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    [Header("Attributes")]
+    public float turnSpeed = 10f;
+    public float range = 10f;
+    public float fireRate = 1f;
+    private float fireCountDown = 0f;
+
+    [Header("Unity Setup")]
+    public string enemyTag = "Enemy";
+    private Transform target;
+    public Transform partToRotate;
+    public GameObject bulletPrefab;
+    public transform firepoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("UpdateTarget", 0f, 0.5f);
+    }
+
+    void UpdateTarget ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance (transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        } 
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else {
+            target = null;
+        } 
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+        {
+            return;
+        }
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation,lookRotation,Time.deltaTime * turnSpeed).eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if(fireCountDown <= 0f)
+        {
+            Shoot();
+            fireCountDown = 1f / fireRate;
+        }
+
+        fireCountDown -= Time.deltaTime;
+    }
+
+    void Shoot ()
+    {
+        Debug.Log("shoot");
+    }
+
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WaveSpawner : MonoBehaviour
+{
+  public Transform enemyPrefab;
+  public TextMeshProUGUI WaveTimer;
+  public Transform spawnPoint;
+  public GameObject Strtbuttn;
+
+  public float WaveTime = 10f;
+  private float countdown = 0f;
+  private int waveIndex = 0;
+  private bool srtGame;
+
+
+  void Start()
+  {
+    srtGame = false;
+  }
+  void Update ()
+  {     
+    if(srtGame == true){
+        if (countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = WaveTime;
+        }
+        countdown -= Time.deltaTime;
+
+        WaveTimer.text = Mathf.Round(countdown).ToString();
+    }
+  }
+
+  IEnumerator SpawnWave()
+  {
+    waveIndex++;
+    for (int i = 0; i < waveIndex; i++)
+    {
+        SpawnEnemy();
+        yield return new WaitForSeconds(0.5f);
+    }
+  }
+
+  void SpawnEnemy()
+  {
+    Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+  }
+
+  public void Srtbuttn()
+  {
+    srtGame = true;
+    Strtbuttn.SetActive(false);
+  }
+}
