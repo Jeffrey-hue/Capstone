5033b62359dd6de1a50caf31d139f50e9087dacc
Jeffrey-hue <76977066+Jeffrey-hue@users.noreply.github.com>
2023-11-13 14:25:45 -0500

    added ability ui and added cooldown

--- Assets/Scripts/NodeUi.cs
+++ Assets/Scripts/NodeUi.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Threading.Tasks;
 
 public class NodeUi : MonoBehaviour
 {
@@ -12,6 +13,74 @@ public class NodeUi : MonoBehaviour
     public TMP_Text sellAmount;
     public Button upgradeButton;
     private Node target;
+    public GameObject LuffyAbility;
+    public GameObject ZoroAbility;
+    public GameObject ChopperAbility;
+    public GameObject RobinAbility;
+    public GameObject UtaAbility;
+    public GameObject Locked;
+    void Update()
+    {
+        LabilityTime -= Time.deltaTime;
+        ZabilityTime -= Time.deltaTime;
+        CabilityTime -= Time.deltaTime;
+        RabilityTime -= Time.deltaTime;
+        UabilityTime -= Time.deltaTime; 
+        if (target.towerBlueprint.name == "Luffy" && target.numOfUpgrades >= 2){
+            LuffyAbility.SetActive(true);
+            ZoroAbility.SetActive(false);
+            ChopperAbility.SetActive(false);
+            RobinAbility.SetActive(false);
+            UtaAbility.SetActive(false);
+            Locked.SetActive(false);
+        }
+
+        if (target.towerBlueprint.name == "Zoro" && target.numOfUpgrades >= 2){
+            LuffyAbility.SetActive(false);
+            ZoroAbility.SetActive(true);
+            ChopperAbility.SetActive(false);
+            RobinAbility.SetActive(false);
+            UtaAbility.SetActive(false);
+            Locked.SetActive(false);
+        }
+
+        if (target.towerBlueprint.name == "Chopper" && target.numOfUpgrades >= 2){
+            LuffyAbility.SetActive(false);
+            ZoroAbility.SetActive(false);
+            ChopperAbility.SetActive(true);
+            RobinAbility.SetActive(false);
+            UtaAbility.SetActive(false);
+            Locked.SetActive(false);
+        }
+
+        if (target.towerBlueprint.name == "Robin" && target.numOfUpgrades >= 2){
+            LuffyAbility.SetActive(false);
+            ZoroAbility.SetActive(false);
+            ChopperAbility.SetActive(false);
+            RobinAbility.SetActive(true);
+            UtaAbility.SetActive(false);
+            Locked.SetActive(false);
+        }
+
+        if (target.towerBlueprint.name == "Uta" && target.numOfUpgrades >= 2){
+            LuffyAbility.SetActive(false);
+            ZoroAbility.SetActive(false);
+            ChopperAbility.SetActive(false);
+            RobinAbility.SetActive(false);
+            UtaAbility.SetActive(true);
+            Locked.SetActive(false);
+        }
+        if (target.numOfUpgrades < 2)
+        {
+            LuffyAbility.SetActive(false);
+            ZoroAbility.SetActive(false);
+            ChopperAbility.SetActive(false);
+            RobinAbility.SetActive(false);
+            UtaAbility.SetActive(false);
+            Locked.SetActive(true);
+        }
+    }
+
     public void SetTarget (Node _target)
     {
         target = _target;
@@ -52,9 +121,59 @@ public class NodeUi : MonoBehaviour
         BuildManager.instance.DeselectNode();
     }
 
-    public void Ability()
+    public float LcoolDown;
+    private float LabilityTime;
+    public float ZcoolDown;
+    private float ZabilityTime;
+    public float CcoolDown;
+    private float CabilityTime;
+    public float RcoolDown;
+    private float RabilityTime;
+    public float UcoolDown;
+    private float UabilityTime;
+    public void LAbility()
     {
-        Debug.Log("Ability");
+        if(LabilityTime <= 0f)
+        {
+            Debug.Log("Ability");
+            LabilityTime = LcoolDown; 
+        }
+    }
+
+    public void ZAbility()
+    {
+        if(ZabilityTime <= 0f)
+        {
+            Debug.Log("Ability");
+            ZabilityTime = ZcoolDown; 
+        }
+    }
+
+    public void CAbility()
+    {
+        if(CabilityTime <= 0f)
+        {
+            Debug.Log("Ability");
+            CabilityTime = CcoolDown; 
+        }
+    }
+
+    public void RAbility()
+    {
+        if(RabilityTime <= 0f)
+        {
+            Debug.Log("Ability");
+            RabilityTime = RcoolDown; 
+        }
+    }
+
+    public void UAbility()
+    {
+        if(UabilityTime <= 0f)
+        {
+            Debug.Log("Ability");
+            UabilityTime = UcoolDown; 
+        }
     }
 
 }
